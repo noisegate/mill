b0c896b4429e574ba94d5aeb39c2ca5c130f36d2
noisegate <marcell.marosvolgyi@gmail.com>
2015-05-24 21:59:15 +0200

    modified:   interface.py 	modified:   mill.py

--- interface.py
+++ interface.py
@@ -1,27 +1,46 @@
 #curses interface for the Mill
 import curses
 import time
+import mill
 
 class Interface(object):
 
     def __init__(self):
         self.screen = curses.initscr()
+        self.size = self.screen.getmaxyx()
+        self.height = self.size[0]
+        self.width = self.size[1]
         curses.cbreak()
         self.callback=None
-        self.win2 = curses.newwin(30, 30, 0, 20)
+
+        self.halfwidth = self.width/2
+        self.halfheight = self.height/2
+
+        self.win2 = curses.newwin(1, self.halfwidth-1 , self.height-1, self.halfwidth-1)
+        self.menu = curses.newwin(1, 1, self.height, self.halfwidth-1)
 
     def main(self):
         self.screen.clear()
-        self.screen.addstr(10,10,"Hello world")
+        #self.screen.box()
+        #self.screen.addstr(10,10,"Hello world")
         self.screen.refresh()
-        self.win2.clear()
+
+    def draw(self):
+        self.menu.box()
+        self.menu.refresh()
         self.win2.box()
         self.win2.refresh()
 
+    def menu(self):
+        self.menu.addstr(1, 1, "MENU")
+
     def loop(self):
         go=1
+        self.draw()
+        #self.menu()
         while(go):
             c = self.screen.getch()
+
             if (c==ord('q')):
                 go=0
             if (c==ord('s')):    
@@ -29,7 +48,6 @@ class Interface(object):
             if (c==ord('c')):
                 self.screen.addstr(12,10,"Invoked callback")
                 self.win2.addstr(0,0,"win")
-                self.win2.refresh()
                 self.callback()
 
     def quit(self):
@@ -37,13 +55,14 @@ class Interface(object):
 
 if __name__ == "__main__":
 
-    def callback():
-        print "H"
-
+    controller = mill.Controll() 
+    controller.loop()
+    exit()
+    
     myscreen =Interface()
+    
     myscreen.main()
-    myscreen.callback = callback
     myscreen.loop()
     myscreen.quit()
-
+    
 
--- mill.py
+++ mill.py
@@ -4,34 +4,73 @@ import time
 import RPi.GPIO as GPIO
 import gcoder
 
+pinbase = 65
+i2c_addr = 0x20
+
 class Controll(object):
 
     def __init__(self):
-        self.y = pololu.Pololu(pololu.Pins(enable=22, direction=24,step=23))
-        self.x = pololu.Pololu(pololu.Pins(enable=26, direction=21,step=20))    
+        # $U3 = X
+        # $U4 = Y
+        self.y = pololu.Pololu(pololu.Pins(enable=15, direction=11,step=13))
+        self.x = pololu.Pololu(pololu.Pins(enable=22, direction=16,step=18))    
         self.x.speed = 120
         self.y.speed = 120
 
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        #GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        #GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        #GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        #GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+        #interrupt
+        GPIO.setup(19,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(19, GPIO.FALLING, callback=self.callback) 
+        
+        self.i2c = wp.I2C()
+        self.dev = self.i2c.setup(i2c_addr)
+         
         wp.wiringPiSetup()
         wp.mcp23016Setup(pinbase, i2c_addr)
+
+        self.state =0
+
+        wp.pinMode(19,0)
+        wp.pullUpDnControl(19,2)
 
         for i in range(65,81):
             wp.pinMode(i,0)
             wp.pullUpDnControl(i,2)
 
-        wp.pinMode(73,0)#pin 0 input
-        wp.pullUpDnControl(73,0)#pull none
+        #wp.pinMode(65, 1)#pin 65 output
+        #wp.digitalWrite(65, 1)#pin 65 high
 
-        wp.pinMode(74,0)#input
+    def callback(self, channel):
+        print "ISR"
+        state = self.i2c.readReg8(self.dev, 0x09)
 
-        wp.pinMode(65, 1)#pin 65 output
-        wp.digitalWrite(65, 1)#pin 65 high
-
+        if ((state&0b1) < (self.state&0b1)):
+            #falling edge channel 1
+            if (not wp.digitalRead(73)):
+                print "DOWN"
+                self.down()
+        
+        if ((state&0b10) < (self.state&0b10)):
+            #falling edge channel 2
+            if (not wp.digitalRead(74)):
+                print "UP"
+                self.up()
+         
+        #if (not wp.digitalRead(74)):
+        #    print "UP" 
+        #    self.up()
+        #if (not wp.digitalRead(75)):
+        #    print "LEFT" 
+        #    self.left()
+        #if (not wp.digitalRead(76)):
+        #    print "DOWN"
+        #    self.down()
+  
     def up(self):
         print "moving up"
         self.x.stepsleft(1)
@@ -45,24 +84,12 @@ class Controll(object):
 
     def right(self):
         self.y.stepsright(1)
-
+   
     def loop(self):
         while(1):
-            if (not wp.digitalRead(73)):
-                print "RIGHT"
-                self.right()
-            if (not wp.digitalRead(74)):
-                print "UP" 
-                self.up()
-            if (not wp.digitalRead(75)):
-                print "LEFT" 
-                self.left()
-            if (not wp.digitalRead(76)):
-                print "DOWN"
-                self.down()
-    
-            time.sleep(0.001)
-
+            time.sleep(0.1)
+            print wp.digitalRead(19)
+   
     def load(self, filename):
         
         try:
@@ -82,6 +109,5 @@ class Controll(object):
 if __name__ == '__main__':
 
     instance = Controll()
-    instance.loop()
 
 
