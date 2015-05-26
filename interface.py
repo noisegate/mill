792f5ce660f8ff4dbc18b5675149b1eebd963d19
noisegate <marcell.marosvolgyi@gmail.com>
2015-05-26 14:55:51 +0200

    modified:   interface.py 	modified:   mill.py

--- interface.py
+++ interface.py
@@ -2,8 +2,24 @@
 import curses
 import time
 import mill
+import fbpy.fb as fb
+
 
 class Interface(object):
+    LEFTCOLUMN = 10
+
+    menudata =  [
+                    [10, LEFTCOLUMN, "MENU"],
+                    [11, LEFTCOLUMN, "===="],
+                    [12, LEFTCOLUMN, "q)      quit"],
+                    [13, LEFTCOLUMN, "c)      user defined"],
+                    [14, LEFTCOLUMN, "s)      MENU"],
+                    [15, LEFTCOLUMN, "o)      set origin"],
+                    [16, LEFTCOLUMN, "l)      load file"],
+                    [17, LEFTCOLUMN, "<space> pause/play"],
+                    [18, LEFTCOLUMN, "i,j,k,m up/dn/lt/rt"],
+                    [19, LEFTCOLUMN, "..."]
+                ]
 
     def __init__(self, controller):
         self.screen = curses.initscr()
@@ -11,6 +27,7 @@ class Interface(object):
         self.height = self.size[0]
         self.width = self.size[1]
         curses.cbreak()
+        curses.noecho()
         self.screen.nodelay(1)#nonblocking fethc getch
         self.callback=None
 
@@ -19,39 +36,63 @@ class Interface(object):
     
         self.controller = controller
 
+        self.surface = fb.Surface()
+        self.graphics = fb.Surface((400,400),(400,400))
+
     def main(self):
         self.screen.clear()
         #self.screen.box()
         #self.screen.addstr(10,10,"Hello world")
         self.screen.refresh()
+        self.graphics.clear()
+        self.graphics.rect((0.0,0.0),(1.0,1.0))
+        self.graphics.update()
 
     def draw(self):
         self.screen.box()
         self.screen.refresh()
 
     def menu(self):
-        pass
+        for line in self.menudata:
+            self.screen.addstr(line[0], line[1],line[2]) 
 
     def loop(self):
         go=1
         self.draw()
-        #self.menu()
+        self.menu()
         while(go):
             c = self.screen.getch()
-
         
-            self.screen.addstr(int(0.9*self.height),2, "MANUAL CTRL:"+controller.movement)
-            self.screen.addstr(int(0.9*self.height)+1,2, "CURR CRD:"+str(controller.xcoord)+":"+str(controller.ycoord))
+            self.screen.addstr( int(0.9*self.height),
+                                self.LEFTCOLUMN, 
+                                "MANUAL CTRL: {0:<10}".format(controller.Movement.names[controller.movement]))
+            self.screen.addstr( int(0.9*self.height)+1,
+                                self.LEFTCOLUMN, 
+                                "CURR CRD: x = {0:<4}  y = {1:<4}".format(str(controller.xcoord), str(controller.ycoord)))
+            
+
+            self.graphics.point((0.5+controller.xcoord/1000.0, 1.0-0.5+controller.ycoord/1000.0))
+            self.graphics.update()
 
             if (c==ord('q')):
                 go=0
-            if (c==ord('s')):    
-                self.screen.addstr(11,10,"Pressed s")
-            if (c==ord('c')):
-                self.screen.addstr(12,10,"Invoked callback")
+            elif (c==ord('s')):   
+                pass
+            elif (c==ord('c')):
                 self.callback()
+            elif (c==ord('o')):
+                self.controller.xcoord=0
+                self.controller.ycoord=0
+            elif (c==ord('i')):
+                self.controller.ycoord-=1
+            elif (c==ord('m')):
+                self.controller.ycoord+=1
+            elif (c==ord('j')):
+                self.controller.xcoord-=1
+            elif (c==ord('k')):
+                self.controller.xcoord+=1
 
-            time.sleep(0.1)
+            time.sleep(0.02)
             controller.handler()
             self.screen.refresh()
 
@@ -61,14 +102,15 @@ class Interface(object):
 if __name__ == "__main__":
 
     def mycallback():
-        print "cb"
+        pass
 
     controller = mill.Controll() 
     
-    myscreen =Interface(controller)
-    myscreen.callback = mycallback
-    myscreen.main()
-    myscreen.loop()
-    myscreen.quit()
+    interface =Interface(controller)
+    
+    interface.callback = mycallback
+    interface.main()
+    interface.loop()
+    interface.quit()
     
 
